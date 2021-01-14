ca92c94a934c79bb45fc29373c131e5f41e19edb
Mirco <verderosamircowork@gmail.com>
2021-01-14 19:02:44 +0100

    Navbar con pulsanti menù a scomparsa,da migliorare eventi in js,possibilità di snellire con una funzione

---
+++ js/script.js
@@ -0,0 +1,60 @@
+// function switched(divclass){
+// $('.divclass').click(function() {
+//
+// $(this).removeClass(switchoff)
+//
+//
+// })
+// return divclass;
+//
+// };
+
+
+
+$(document).ready(function() {
+
+// PULSANTE FEATURES PER DROPDOWN MENU
+  $('.press-for-dropdown1').click(function() {
+
+    $('.box-dropdown1').toggle()
+
+  });
+
+
+
+// PULSANTE PLANS PER DROPDOWN MENU
+  $('.press-for-dropdown2').click(function() {
+
+    $('.box-dropdown2').toggle()
+
+  });
+
+
+
+// PULSANTE ABOUT US PER DROPDOWN MENU
+  $('.press-for-dropdown3').click(function() {
+
+    $('.box-dropbox3').toggle()
+
+  });
+
+
+
+// PULSANTE HELP PER DROPDOWN MENU
+  $('.press-for-dropdown4').click(function() {
+
+    $('.box-dropbox4').toggle()
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+});
