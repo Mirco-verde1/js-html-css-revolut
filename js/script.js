7f368b9573f8700800d33714ca9db0c95766a52a
mirco verderosa <verderosamircowork@gmail.com>
2021-02-01 18:47:53 +0100

    refactoring

--- js/script.js
+++ js/script.js
@@ -1,33 +1,7 @@
-// function switched(divclass){
-// $('.divclass').click(function() {
-//
-// $(this).removeClass(switchoff)
-//
-//
-// })
-// return divclass;
-//
-// };
-//
-
-// function switched(divclass1 , divclass2){
-//   $('.divclass1').click(function() {
-//
-//     $('.divclass2').toggle(900)
-//
-//   })
-//   return switched;
-// };
-
-//
-// function colorGrey(classname){
-//   $('.classname').css('color','grey')
-//   return colorGrey;
-// };
 
 $(document).ready(function() {
 
-// PULSANTE FEATURES PER DROPDOWN MENU
+  // PULSANTE FEATURES PER DROPDOWN MENU
   $('.press-dropdown1').click(function() {
 
     $('.box-dropdown1').toggle(900)
@@ -36,7 +10,7 @@ $(document).ready(function() {
 
 
 
-// PULSANTE PLANS PER DROPDOWN MENU
+  // PULSANTE PLANS PER DROPDOWN MENU
   $('.press-dropdown2').click(function() {
 
     $('.box-dropdown2').toggle(900)
@@ -45,30 +19,20 @@ $(document).ready(function() {
 
 
 
-// PULSANTE ABOUT US PER DROPDOWN MENU
+  // PULSANTE ABOUT US PER DROPDOWN MENU
   $('.press-dropdown3').click(function() {
 
     $('.box-dropbox3').toggle(900)
 
   });
 
+  s
 
-
-// PULSANTE HELP PER DROPDOWN MENU
+  // PULSANTE HELP PER DROPDOWN MENU
   $('.press-dropdown4').click(function() {
 
     $('.box-dropbox4').toggle(900)
 
   });
-
-
-
-
-
-
-
-
-
-
 
 });
